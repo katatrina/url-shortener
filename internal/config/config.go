696d0f96c41ed629159d2a6d3f3007186f700a11
katatrina <cvphuoc2014@gmail.com>
2026-06-01 05:59:38 +0700

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
+)
+
+// Config .
+type Config struct {
+}
+
+// Load .
+func Load() (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
