8efc8b977a239c9dca55ef7a47057a04e94c98dc
katatrina <cvphuoc2014@gmail.com>
2026-06-16 08:21:58 +0700

    chore: add fly.io deploy config, drop healthz endpoint

--- internal/httpserver/router.go
+++ internal/httpserver/router.go
@@ -1,8 +1,6 @@
 package httpserver
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/katatrina/url-shortener/internal/httpserver/middleware"
@@ -15,18 +13,18 @@ func NewRouter(db *pgxpool.Pool) *gin.Engine {
 
 	r.Use(middleware.RequestID(), middleware.AccessLog(), middleware.Recovery())
 
-	r.GET("/healthz", healthz(db))
+	//r.GET("/healthz", healthz(db))
 
 	return r
 }
 
-func healthz(db *pgxpool.Pool) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		if err := db.Ping(c.Request.Context()); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"status": "service unavailable"})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"status": "ok"})
-	}
-}
+//func healthz(db *pgxpool.Pool) gin.HandlerFunc {
+//	return func(c *gin.Context) {
+//		if err := db.Ping(c.Request.Context()); err != nil {
+//			c.JSON(http.StatusInternalServerError, gin.H{"status": "service unavailable"})
+//			return
+//		}
+//
+//		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+//	}
+//}
